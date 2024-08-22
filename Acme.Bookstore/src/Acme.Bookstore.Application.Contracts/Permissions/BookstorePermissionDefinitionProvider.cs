4fc96b5c8c4b6685061daaf27ebcb3bca14a22b1
Gonzalo Flores Chuchon <gonzalo.flores@semicrol.com>
2024-08-22 14:59:39 +0200

    Permission Definitions & Permission Management UI / new Translations for Permissions

--- Acme.Bookstore/src/Acme.Bookstore.Application.Contracts/Permissions/BookstorePermissionDefinitionProvider.cs
+++ Acme.Bookstore/src/Acme.Bookstore.Application.Contracts/Permissions/BookstorePermissionDefinitionProvider.cs
@@ -1,17 +1,22 @@
 ﻿using Acme.Bookstore.Localization;
+using Acme.Bookstore.Permissions;
 using Volo.Abp.Authorization.Permissions;
 using Volo.Abp.Localization;
 
-namespace Acme.Bookstore.Permissions;
+namespace Acme.BookStore.Permissions;
 
 public class BookstorePermissionDefinitionProvider : PermissionDefinitionProvider
 {
     public override void Define(IPermissionDefinitionContext context)
     {
-        var myGroup = context.AddGroup(BookstorePermissions.GroupName);
-        //Define your own permissions here. Example:
-        //myGroup.AddPermission(BookstorePermissions.MyPermission1, L("Permission:MyPermission1"));
+        var bookStoreGroup = context.AddGroup(BookstorePermissions.GroupName, L("Permission:BookStore"));
+
+        var booksPermission = bookStoreGroup.AddPermission(BookstorePermissions.Books.Default, L("Permission:Books"));
+        booksPermission.AddChild(BookstorePermissions.Books.Create, L("Permission:Books.Create"));
+        booksPermission.AddChild(BookstorePermissions.Books.Edit, L("Permission:Books.Edit"));
+        booksPermission.AddChild(BookstorePermissions.Books.Delete, L("Permission:Books.Delete"));
     }
+
 
     private static LocalizableString L(string name)
     {
