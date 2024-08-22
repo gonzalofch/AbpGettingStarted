e3099c97b96ad38d13f53d77d51d0d87e84e667d
Gonzalo Flores Chuchon <gonzalo.flores@semicrol.com>
2024-08-22 14:38:01 +0200

    Integration Tests - For BookAppService

---
+++ Acme.Bookstore/test/Acme.Bookstore.EntityFrameworkCore.Tests/EntityFrameworkCore/Applications/Books/EfCoreBookAppService_Tests.cs
@@ -0,0 +1,9 @@
+using Acme.Bookstore.Books;
+using Xunit;
+
+namespace Acme.Bookstore.EntityFrameworkCore.Applications.Books;
+
+public class EfCoreBookAppService_Tests : BookAppService_Tests<BookstoreEntityFrameworkCoreTestModule>
+{
+    
+}
