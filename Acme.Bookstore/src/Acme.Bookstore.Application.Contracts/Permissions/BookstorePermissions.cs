209477d58a9f4d70c91527ca59f0ef5b59c20c1c
Gonzalo Flores Chuchon <gonzalo.flores@semicrol.com>
2024-08-22 14:48:20 +0200

    Authorization - Permission Names

--- Acme.Bookstore/src/Acme.Bookstore.Application.Contracts/Permissions/BookstorePermissions.cs
+++ Acme.Bookstore/src/Acme.Bookstore.Application.Contracts/Permissions/BookstorePermissions.cs
@@ -2,8 +2,15 @@
 
 public static class BookstorePermissions
 {
-    public const string GroupName = "Bookstore";
+    public const string GroupName = "BookStore";
 
     //Add your own permission names. Example:
     //public const string MyPermission1 = GroupName + ".MyPermission1";
+    public static class Books
+    {
+        public const string Default = GroupName + ".Books";
+        public const string Create = Default + ".Create";
+        public const string Edit = Default + ".Edit";
+        public const string Delete = Default + ".Delete";
+    }
 }
