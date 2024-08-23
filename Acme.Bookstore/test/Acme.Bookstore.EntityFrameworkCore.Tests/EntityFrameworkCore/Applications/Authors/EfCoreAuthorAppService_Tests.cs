92654f9252b02728e012c2e69b4bb040e91155dc
Gonzalo Flores Chuchon <gonzalo.flores@semicrol.com>
2024-08-23 13:09:33 +0200

    Authors AppService Tests

---
+++ Acme.Bookstore/test/Acme.Bookstore.EntityFrameworkCore.Tests/EntityFrameworkCore/Applications/Authors/EfCoreAuthorAppService_Tests.cs
@@ -0,0 +1,8 @@
+using Acme.BookStore.Authors;
+
+namespace Acme.Bookstore.EntityFrameworkCore.Applications.Authors;
+
+public class EfCoreAuthorAppService_Tests : AuthorAppService_Tests<BookstoreEntityFrameworkCoreTestModule>
+{
+    
+}
